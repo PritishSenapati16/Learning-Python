941ca18de7b22729cd27553e38c2b7cb83798935
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-18 00:05:30 +0530

    Add files via upload

---
+++ practise set2 q5 .py
@@ -0,0 +1,9 @@
+# Q5-- Write a python program to find an average of two numbers entered by the user.
+
+#Ans -- 
+#signing in
+a = int(input("enter a number 1"))
+b = int(input("enter a number 2"))
+
+print("the average of these two numbers is",(a+b)/2)
+#signing off
---
+++ practise set2 q6 .py
@@ -0,0 +1,7 @@
+# Write a python program to calculate the square of a number entered by the user.
+
+#Ans--
+#signing in
+a = int(input("enter a number k"))
+print("square of number is",a*a)
+#signing off
