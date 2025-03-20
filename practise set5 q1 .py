d77129cd469a8ef2871000283b273a26a8317a7d
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-20 23:39:52 +0530

    Add files via upload

---
+++ practise set5 q1 .py
@@ -0,0 +1,13 @@
+# Q1 --- Write a program to create a dictionary of Hindi words with values as their English translation.Provide user with an option to look it up.
+
+# Ans ---
+# signing in 
+words = {
+    "madad":"Help",
+    "kursi":"chair",
+    "pyar" :"Khusi"
+}
+word = input("enter the word u want meaning of :")
+
+print(words[word])
+# signing off 
