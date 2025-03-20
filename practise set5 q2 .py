d150b9ed40e76915722331331be4a35e391463c2
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-20 23:40:09 +0530

    Add files via upload

---
+++ practise set5 q2 .py
@@ -0,0 +1,25 @@
+# Q2 --- Write a program to input eight numbers from the user and display all the unique numbers(once).
+
+# Ans --- 
+# signing in 
+s = set ()
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+n = input("enter a numbder")
+s.add(int(n))
+print(s)
+# signing off
+
+
