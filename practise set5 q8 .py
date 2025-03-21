3e18abd66f22093362ae84c0eb1c4621c5651b2d
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-22 00:13:36 +0530

    Add files via upload

---
+++ practise set5 q8 .py
@@ -0,0 +1,25 @@
+# Q8 --- If languages of two friends are same ; what will happen to the program in problem 6 ?
+
+# Ans --- 
+# signing in
+d = {}
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+print(d)
+# signing off
+
+# Even if the languages of 2 friends are same , then also it will be printed.
