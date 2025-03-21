ff9d76207b5fab9bb76f23180d77325a8b834b75
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-21 19:32:09 +0530

    Add files via upload

---
+++ practise set5 q4 .py
@@ -0,0 +1,15 @@
+# Q4 --- What will be the length of following set s :
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')
+
+# Ans --- 
+# signing in
+s = set()
+s.add(20)
+s.add(20.0)
+s.add("20")
+print(len(s))
+# signing off
+# In PYTHON , the value of floating point number and an intezer is determined only numericaLly i.e. 20.0 == 20 
