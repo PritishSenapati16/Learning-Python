ffe457b71650f6fcaf8c115b3c5ed3f026a12afc
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-21 19:33:06 +0530

    Add files via upload

---
+++ practise set5 q6 .py
@@ -0,0 +1,23 @@
+# Q6 --- Create an empty dictionary.Allow four friends to enter their favourite language as value and use key as their names.Assume that the names are unique.
+
+# Ans ---
+# signing in 
+d = {}
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+print(d)
+# signing off
