c44581f833a2b9752cbb29930160ff952e276e45
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-23 18:49:23 +0530

    Add files via upload

---
+++ practise set6 q5 .py
@@ -0,0 +1,15 @@
+# Q5 --- Write a program which finds out whether a given name is present in a list or not.
+
+# Ans ---
+# signing in 
+L = ["Spy","EvilKr4tos","Bombastic","Futuror"]
+
+name = input("enter your name:")
+
+if(name in L):
+    print("Your name in the list")
+
+else:
+    print("Get Out")
+print("END OF PROGRAM")
+# signing off     
