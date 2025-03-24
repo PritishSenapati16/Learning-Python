3e7cbee5eac884141f7744cc779a4e4f2bd663c4
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-24 23:13:49 +0530

    Add files via upload

---
+++ practise set6 q7 .py
@@ -0,0 +1,13 @@
+# Q7 --- Write a program to find out whether a given post is talking about "Khusi"or not.
+
+# Ans --- 
+# signing in 
+post = input("enter the post:")
+
+if("Khusi".lower() in post.lower()):
+    print("This post is talking about Khusi")
+else:
+    print("No Khusi")
+print("END OF PROGRAM")
+# signing off
+
