8961380008677108345d144e50c7ad194c47632a
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-24 23:17:00 +0530

    Add files via upload

---
+++ practise set7 q3 .py
@@ -0,0 +1,14 @@
+# Q3 --- Attempt the number 1 question using while loop . 
+
+# Ans---
+# signing in
+n = int(input("enter a number:"))
+
+i = 1 
+
+while(i<11):
+    print(f"{n} x {i} = {n*i}")
+    i = i+1
+
+print("end of program")
+# signing off 
