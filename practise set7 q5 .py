e4a2595722a3737a7ba726bf4c9f2e2e63796e72
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-25 23:02:03 +0530

    Add files via upload

---
+++ practise set7 q5 .py
@@ -0,0 +1,14 @@
+# Q5 --- Write a program to find the sum of first n natural numbers using while loop .
+
+# Ans ---
+# signing in
+n = int(input("enter the number:"))
+i = 1 
+sum = 0
+while(i<=n):
+    sum += i
+    i = i + 1
+
+print(sum)
+print("end of program")
+# signing off
