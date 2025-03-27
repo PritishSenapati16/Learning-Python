de61451d11ed9431fcc8c27c333a05d4b5ef2863
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-27 17:13:01 +0530

    Add files via upload

---
+++ practise set7 q8 .py
@@ -0,0 +1,15 @@
+# Q8 --- Print the following star pattern
+'''
+*
+**
+*** for n = 3
+'''
+
+# Ans --- 
+# signing in 
+n = int(input("enter a number:"))
+for i in range(1, n+1):
+    print("*"* i, end = "")
+    print("")
+# signing off
+
