7c44d303b54567e91588372456373392fa0c328c
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-28 20:42:41 +0530

    Add files via upload

---
+++ practise set8 q2 .py
@@ -0,0 +1,10 @@
+# Q2 --- Write a python program using function to convert celcius to fahrenheit.
+
+# Ans ---
+# signing in 
+def c_to_f(c):
+    return (c*9/5)+32
+
+c = int(input("enter temperature in C : "))
+print(c_to_f(c))
+# signing off
