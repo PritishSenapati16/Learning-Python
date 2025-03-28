7f47b257969e96c3e6046e01017ca11afe8b8442
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-28 20:49:48 +0530

    Add files via upload

---
+++ practise set8 q6 .py
@@ -0,0 +1,12 @@
+# Q6 --- Write a python function which converts inches to cms.and
+
+# Ans ---
+# signing in 
+def inch_to_cms(inch):
+    return inch * 2.54
+
+n = int(input("enter the value in inches:"))
+
+print(f"The corresponding value in cms is {inch_to_cms(n)}")
+
+# signing off
