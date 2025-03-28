9d2b3130be25da93750163f2425d1c96fff2bd99
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-28 20:43:06 +0530

    Add files via upload

---
+++ practise set8 q3 .py
@@ -0,0 +1,9 @@
+# Q3 --- How to prevent a python print() function to print a new line at the end .
+
+# Ans --- 
+# signing in 
+print("a")
+print("b")
+print("c" , end = "")
+print("hutiya")
+# signing off
