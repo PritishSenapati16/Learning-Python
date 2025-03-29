20d53a93e21843ca93bf2a9f0146bb9099dc02fb
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-29 10:37:06 +0530

    Add files via upload

---
+++ practise set8 q8 .py
@@ -0,0 +1,11 @@
+# Q8 --- Write a python function to print multiplication table of a given number .
+
+# Ans ---
+# signing in 
+def multiply(n):
+    for i in range(1,11):
+        print(f"{n} x {i} = {n*i}")
+
+multiply(5)
+# signing off
+
