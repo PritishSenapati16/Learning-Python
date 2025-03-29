5b2f1ed5ca70fa08f1ccaa2f1c94f8755feb48dc
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-29 10:37:37 +0530

    Add files via upload

---
+++ practise set9 q1 .py
@@ -0,0 +1,12 @@
+# Q1 --- Write a program to read a text from a given file 'poem.txt'and find out whether it contains the word 'twinkle'.
+
+# Ans ---
+# signing in
+f = open("poem.txt")
+if ("twinkle in content"):
+    print("The word twinkle is present in the content")
+
+else:
+    print("BYE")
+f.close()
+# signing off 
