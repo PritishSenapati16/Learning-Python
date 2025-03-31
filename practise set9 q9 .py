cbfffea299b24e013317e98897c391ee68c6d01f
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-03-31 14:38:12 +0530

    Add files via upload

---
+++ practise set9 q9 .py
@@ -0,0 +1,19 @@
+# Q9 --- Write a program to find out whether a file is identical & matches the content of another file.
+
+# Ans ---
+# signing in 
+with open("cute.txt") as f:
+    content1 = f.read()
+
+with open("Donkey.txt") as f :
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Yes these files are identical")
+
+else:
+    print("No these files are not identical")
+
+print("End of program")
+# signing off         
+          
