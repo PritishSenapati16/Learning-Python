0dd444d9eb3c5342db2e53e3cf120ad9eaffdea2
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-04-21 03:41:14 +0530

    Add files via upload

---
+++ practise set11 q3 .py
@@ -0,0 +1,28 @@
+# Q3 - Create a class 'Employee' and add salary and increment properties to it.
+# "Write a method salaryAfterIncrement method with a @property decorator with a setter which changes the value of increment based on the salary."
+
+# ANS ---
+# Signing in
+class Employee:
+    salary = 643
+    increment = 68
+    @property
+    def salaryAfterIncrement(self):
+        return (self.salary + self.salary * (self.increment/100))
+    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self,salary):
+        self.increment = ((salary/self.salary)-1)*100
+
+
+e = Employee()
+print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 1080.24
+print(e.increment)
+print("END OF THE PROGRAM")
+# Signing off 
+
+
+
+    
+
